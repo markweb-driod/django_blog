459a933e43f7549666bebf3000b8433a70cf4315
markweb-driod <markishayajatau007@gmail.com>
2025-01-19 15:24:39 +0100

    user signup done

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
+from django.urls import reverse_lazy
+from django.views import generic
+# Create your views here.b 
 
-# Create your views here.
+class CreateUser(generic.CreateView):
+    success_url = reverse_lazy('homepage')
+    form_class = UserCreationForm
+    template_name = 'signup.html'
